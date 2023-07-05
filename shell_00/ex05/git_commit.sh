2b7c54d2c86e2e7e9547175fba28496369966ae9
Eduardo Botelho <79218663+kaaos@users.noreply.github.com>
2023-07-05 03:06:36 -0300

    Update git_commit.sh

--- shell_00/ex05/git_commit.sh
+++ shell_00/ex05/git_commit.sh
@@ -1,2 +1,2 @@
-#! /bin/sh                                                                      
+#! /bin/bash/sh                                                                      
 git log --format='%H' -n5
